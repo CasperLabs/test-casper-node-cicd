b71df0a5ab1e3247e9780f9187815e096758acf5
Piotr Skowron <piotr@pop-os.localdomain>
2020-10-09 01:05:47 +0200

    as script

--- update-rev.sh
+++ update-rev.sh
@@ -5,7 +5,7 @@ DEST_FILE='node/Cargo.toml'
 
 if [ -f $DEST_FILE ]; then 
   echo "[INFO] Going to update $DEST_FILE with revision set to $DRONE_BUILD_NUMBER"
-  sed -i s'/^revision =.*/revision = "'"${DRONE_BUILD_NUMBER}"'"/' $DEST_FILE
+  sed -i s'/^revision =.*/revision = \"'"${DRONE_BUILD_NUMBER}"'\"/' $DEST_FILE
   cat $DEST_FILE
 else
   echo "[ERRPR] Unable to find: $DEST_FILE"
